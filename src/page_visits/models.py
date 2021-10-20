c764717dd6ce4a5fb8a7734e8f7f6106f6838019
Henry Pulver <henrypulver@hotmail.co.uk>
2021-10-20 11:48:25 +0100

    Make session_id editable

--- src/page_visits/models.py
+++ src/page_visits/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class PageVisitModel(models.Model):
     user_id = models.TextField()
-    session_id = models.TextField(
-        blank=True, editable=False, help_text="session_key of the session"
-    )
+    session_id = models.TextField(blank=True, help_text="session_key of the session")
     page_extension = models.TextField(default="", help_text="URL extension of the page visited")
     timestamp = models.DateTimeField(auto_now=True)
