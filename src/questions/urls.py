4f9ed29bb3671817e73bcdb3d4016b6720ef046a
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-03 15:42:38 +0000

    Add questions app urls

---
+++ src/questions/urls.py
@@ -0,0 +1,23 @@
+"""question_bot URL Configuration.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from questions.views import QuestionResponseView, QuestionView
+
+urlpatterns = [
+    path("api/v0/questions", QuestionView.as_view(), name="questions"),
+    path("api/v0/question_response", QuestionResponseView.as_view(), name="question_responses"),
+]
