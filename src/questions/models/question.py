61a3d2cb4a0a29e8f6abfecaee57059a913849ba
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-03 17:59:02 +0000

    Add validators to question fields to ensure not empty

--- src/questions/models/question.py
+++ src/questions/models/question.py
@@ -2,17 +2,25 @@ from uuid import uuid4
 
 from django.db import models
 
+from questions.validators import ensure_list, not_null
+
 
 class QuestionModel(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid4, help_text="Unique id for question response"
     )
 
-    question_text = models.TextField(help_text="Text for question")
-    answer_text = models.JSONField(
-        help_text="JSON list containing answers - first is correct, all after are incorrect."
+    question_text = models.TextField(
+        help_text="Text for question", blank=False, validators=[not_null]
     )
-    feedback_text = models.TextField(help_text="Text containing feedback")
+    answer_text = models.JSONField(
+        help_text="JSON list containing answers - first is correct, all after are incorrect.",
+        blank=False,
+        validators=[not_null, ensure_list],
+    )
+    feedback_text = models.TextField(
+        help_text="Text containing feedback", blank=False, validators=[not_null]
+    )
 
     author_user_id = models.CharField(
         max_length=64, help_text="User ID of user who wrote this question"
@@ -20,6 +28,6 @@ class QuestionModel(models.Model):
     session_id = models.TextField(
         null=True, blank=True, help_text="session_id of the session the question was written in"
     )
-    time_written = models.DateTimeField(
+    timestamp = models.DateTimeField(
         auto_now_add=True, help_text="Time that the question was written"
     )
