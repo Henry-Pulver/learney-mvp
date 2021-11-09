c460296f7228d00e234b66895aa17f2edb992527
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-09 20:31:06 +0000

    Remove question_bot

--- src/question_bot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuestionBotConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "question_bot"
--- src/question_bot/models/question_last_asked.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class QuestionLastAskedModel(models.Model):
-    user_email = models.CharField(max_length=128)
-    time_asked = models.DateTimeField(auto_now=True)
