1bb7f88eae326206f988975f9e003ba195b465dc
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-24 22:23:25 +0000

    Add admin to knowledge_maps model

--- src/knowledge_maps/admin.py
+++ src/knowledge_maps/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from knowledge_maps.models import KnowledgeMapModel
+
+
+@admin.register(KnowledgeMapModel)
+class QuestionModelAdmin(admin.ModelAdmin):
+    pass
