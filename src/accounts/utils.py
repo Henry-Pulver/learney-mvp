42341496303f88400ab3b56f5aee13620faab020
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-13 18:54:39 +0000

    Make all optional user data optional!

--- src/accounts/utils.py
+++ src/accounts/utils.py
@@ -4,10 +4,10 @@ from typing import Dict
 def user_data_to_user_db_object(user_data: Dict) -> Dict:
     return {
         "id": user_data["sub"],
-        "name": user_data["name"],
-        "nickname": user_data["nickname"],
+        "name": user_data.get("name", ""),
+        "nickname": user_data.get("nickname", ""),
         "given_name": user_data.get("given_name", ""),
-        "family_name": user_data["family_name"],
-        "picture": user_data["picture"],
-        "locale": user_data["locale"],
+        "family_name": user_data.get("family_name", ""),
+        "picture": user_data.get("picture", ""),
+        "locale": user_data.get("locale", ""),
     }
