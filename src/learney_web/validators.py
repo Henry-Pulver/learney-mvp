b742057a999caedd6f946fc93b756fccc1ee2768
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-20 15:52:09 +0000

    Add hex colour validator (for topics when we add them oops)

--- src/learney_web/validators.py
+++ src/learney_web/validators.py
@@ -1,6 +1,13 @@
+import re
+
 from django.core.exceptions import ValidationError
 
 
 def validate_numeric(value: str) -> None:
     if not value.isnumeric():
         raise ValidationError(f"{value} is not a number string!")
+
+
+def validate_hex_colour(value: str) -> None:
+    if not re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", value):
+        raise ValidationError(f"{value} is not a valid hex colour!")
