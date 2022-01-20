37b395e7d73666972f704da37871b6e721933720
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-20 15:55:10 +0000

    Add question template DB model

---
+++ src/questions/models/question_template.py
@@ -0,0 +1,47 @@
+from uuid import uuid4
+
+from django.db import models
+
+from knowledge_maps.models import Concept
+from learney_backend.models import UUIDModel
+from questions.validators import integer_is_positive, not_null
+
+
+class QuestionTemplateModel(UUIDModel):
+    concept = models.ForeignKey(
+        Concept,
+        related_name="question_templates",
+        help_text="Concept that the question corresponds to",
+        on_delete=models.CASCADE,
+    )
+    difficulty = models.FloatField(
+        help_text="Question difficulty for the concept. Initially set by an expert, but will subsequently be inferred"
+        " from data. A relative scale, with 0 the lowest possible and as many difficulty levels as is deemed"
+        " makes sense by the expert.",
+        validators=[integer_is_positive],
+    )
+    question_type = models.TextField(
+        help_text="Text for question template - generates full question",
+        blank=False,
+        validators=[not_null],
+    )
+
+    template_text = models.TextField(
+        help_text="Text for question template - generates full question",
+        blank=False,
+        validators=[not_null],
+        max_length=16384,
+    )
+
+    # map = models.ForeignKey(
+    #     KnowledgeMapModel,
+    #     on_delete=models.SET_NULL,
+    #     help_text="The map the concept is tagged to",
+    # )
+    # session_id = models.TextField(
+    #     null=True, blank=True, help_text="session_id of the session the question was written in"
+    # )
+    # author_user_id = models.ForeignKey(User, help_text="User who wrote this question", null=True, blank=True)
+    # timestamp = models.DateTimeField(
+    #     auto_now_add=True, help_text="Time that the question was written"
+    # )
--- src/questions/validators.py
+++ src/questions/validators.py
@@ -3,6 +3,11 @@ from typing import Any
 from django.core.exceptions import ValidationError
 
 
+def integer_is_positive(integer: int) -> None:
+    if integer < 0:
+        raise ValidationError("Value isn't positive!")
+
+
 def not_null(value: str) -> None:
     if not value:
         raise ValidationError("Value is null!")
