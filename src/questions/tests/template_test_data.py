ee764ad7a9b4a65f776ebf99b939ab7c2f599d84
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-24 22:05:54 +0000

    Add data to test the parser functions on

---
+++ src/questions/tests/template_test_data.py
@@ -0,0 +1,36 @@
+QUESTION_TEMPLATE_STRING = r"""param A: {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+param B: {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+$$\mathbf{A}$$ is a $$<<A>> \times <<B>>$$ matrix, and $$\mathbf{B}$$ is a $$<<B>> \times <<A>>$$ matrix.
+What shape matrix results from $$\mathbf{A}\times\mathbf{B}$$?
+
+a) $$<<B>> \times <<B>>$$
+b) $$<<A>> \times <<A>>$$
+c) $$<<A>> \times <<B>>$$
+d) $$<<A * A>> \times <<B * B>>$$
+
+Feedback
+In matrix multiplication, the order of operation matters (i.e. A x B ≠ B x A).
+We take the rows of the first matrix, and multiply them by the columns of the second.
+In practice, this means the size of the output matrix will be $$n(rows(A)) \times n(columns(B))$$
+In this case, that means an <<A>> by <<A>> matrix."""
+
+PARAMS_DICT = {"A": "4", "B": "7"}
+
+QUESTION_TEXT = r"""$$\mathbf{A}$$ is a $$4 \times 7$$ matrix, and $$\mathbf{B}$$ is a $$7 \times 4$$ matrix.
+What shape matrix results from $$\mathbf{A}\times\mathbf{B}$$?
+"""
+
+ANSWERS = {
+    "a": r"$$7 \times 7$$",
+    "b": r"$$4 \times 4$$",
+    "c": r"$$4 \times 7$$",
+    "d": r"$$16 \times 49$$",
+}
+
+FEEDBACK = r"""In matrix multiplication, the order of operation matters (i.e. A x B ≠ B x A).
+We take the rows of the first matrix, and multiply them by the columns of the second.
+In practice, this means the size of the output matrix will be $$n(rows(A)) \times n(columns(B))$$
+In this case, that means an 4 by 4 matrix.
+"""
