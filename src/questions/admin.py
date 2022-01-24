87d89b19498a1e93f516ed094d2548be2eef6f8d
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-24 22:06:29 +0000

    Fix admin panel

--- src/questions/admin.py
+++ src/questions/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from questions.models import QuestionResponseModel, QuestionTemplate
+from questions.models import QuestionTemplate
 
 
 @admin.register(QuestionTemplate)
 class QuestionTemplateModelAdmin(admin.ModelAdmin):
     list_select_related = ("concept",)
-    list_display = ("id", "concept__name", "question_type", "difficulty", "last_updated")
+    list_display = ("id", "concept", "question_type", "difficulty", "last_updated")
     search_fields = ["id", "question_type", "template_text", "difficulty", "concept"]
