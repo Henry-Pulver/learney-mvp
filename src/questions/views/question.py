33c7143091c9c8f8795915d727545c29600a0273
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-25 16:38:32 +0000

    Add getting a question view

--- src/questions/views/question.py
+++ src/questions/views/question.py
@@ -1,81 +1,68 @@
-# import json
-# from random import shuffle
-#
-# from rest_framework import status
-# from rest_framework.request import Request
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-#
-# from questions.models import QuestionTemplateModel
-#
-# MAX_NUMBER_OF_QUESTIONS = 5
-#
-#
-# class QuestionView(APIView):
-#     def get(self, request: Request, format=None) -> Response:
-#         try:
-#             user_id = request.GET["user_id"]
-#             concept_id = request.GET["concept_id"]
-#             map_uuid = request.GET["map_uuid"]
-#
-#             # Get all relevant questions
-#             all_questions = QuestionTemplateModel.objects.prefetch_related("tags").prefetch_related(
-#                 "responses"
-#             )
-#             concept_questions = all_questions.filter(
-#                 tags__concept_id=concept_id, tags__map__unique_id=map_uuid
-#             )
-#
-#             unanswered_questions = concept_questions.filter(responses=None).order_by("?")
-#             user_responses = concept_questions.filter(responses__user_id=user_id)
-#             incorrectly_answered_questions = user_responses.filter(
-#                 responses__correct=False
-#             ).order_by("responses__time_asked")
-#             correctly_answered_questions = user_responses.filter(responses__correct=True).order_by(
-#                 "responses__time_asked"
-#             )
-#
-#             # Select the next question set from possible options
-#             questions_chosen = []
-#             for question_entry in [
-#                 entry
-#                 for query_set in [
-#                     unanswered_questions,
-#                     incorrectly_answered_questions,
-#                     correctly_answered_questions,
-#                 ]
-#                 for entry in query_set
-#             ]:
-#                 if question_entry not in questions_chosen:
-#                     questions_chosen.append(question_entry)
-#                 if len(questions_chosen) == MAX_NUMBER_OF_QUESTIONS:
-#                     break
-#
-#             num_mistakes_allowed = round(
-#                 sum(1 / len(question.answer_text) for question in questions_chosen) / 2
-#             )
-#             question_serializers = [QuestionSerializer(q) for q in questions_chosen]
-#
-#             response_payload = {
-#                 "correct_threshold": len(questions_chosen) - num_mistakes_allowed,
-#                 "question_set": [ser.data for ser in question_serializers],
-#             }
-#             return Response(response_payload, status=status.HTTP_200_OK)
-#         except KeyError as error:
-#             return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
-#
-#     def post(self, request: Request, format=None) -> Response:
-#         # Add question
-#         serializer = QuestionSerializer(data=request.data)
-#         if not serializer.is_valid():
-#             return Response(str(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
-#         saved_model = serializer.save()
-#         # Add tags
-#         for tag in json.loads(request.data["tags"]):
-#             QuestionTagModel.objects.create(
-#                 question_id=saved_model.id,
-#                 map_uuid=request.data["map_uuid"],
-#                 concept_id=tag,
-#             )
-#
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+import numpy as np
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from questions.models import QuestionResponse, QuestionTemplate
+from questions.models.inferred_knowledge_state import InferredKnowledgeState
+from questions.models.question_set import QuestionSet
+from questions.question_selection import difficulty_terms, novelty_terms
+from questions.template import parse_params, question_from_template, sample_params
+
+
+class QuestionView(APIView):
+    def get(self, request: Request, format=None) -> Response:
+        try:
+            concept_id = request.GET["concept_id"]
+            user_id = request.GET["user_id"]
+            question_set = QuestionSet.objects.get(
+                id=request.GET["question_set_id"]
+            ).selected_related("responses")
+
+            knowledge_state = InferredKnowledgeState.objects.get(
+                user=user_id, concept=concept_id
+            ).select_related("user")
+
+            template_options = QuestionTemplate.objects.filter(
+                concept__id=concept_id
+            ).prefetch_related("responses")
+
+            # Calculate the weights. Once normalised, these form the categorical
+            #  distribution over question templates
+            question_weights = difficulty_terms(template_options, knowledge_state) * novelty_terms(
+                template_options, knowledge_state.user, question_set
+            )
+
+            # Choose the template that's going to be used!
+            question_seen_before = True
+            while question_seen_before:
+                chosen_template = np.random.choice(
+                    template_options, p=question_weights / np.mean(question_weights)
+                )
+                question_param_options, remaining_text = parse_params(chosen_template.template_text)
+                sampled_params = sample_params(question_param_options)
+                # Make 100% sure it's not been seen already in this question set!
+                question_seen_before = question_set.responses.filter(
+                    question_template=chosen_template, question_params=sampled_params
+                ).exists()
+
+            question = question_from_template(chosen_template.id, remaining_text, sampled_params)
+
+            # Track the question was sent in the DB
+            QuestionResponse.objects.create(
+                user=user_id,
+                question_template=chosen_template,
+                question_params=sampled_params,
+                question_set=question_set,
+                session_id=request.GET["session_id"],
+                response=None,
+                correct=None,
+                time_to_respond=None,
+            )
+
+            # TODO: Track with Mixpanel
+
+            return Response(question, status=status.HTTP_200_OK)
+        except KeyError as error:
+            return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
