b5a6d8a17798645eb1f0ab50e9a74553f66c36ef
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-26 16:11:57 +0000

    Fix potential bug where knowledge state didn't already exist

--- src/questions/views/concept_info.py
+++ src/questions/views/concept_info.py
@@ -5,12 +5,24 @@ from rest_framework.views import APIView
 
 from questions.models.inferred_knowledge_state import InferredKnowledgeState
 
+INIT_MEAN = 1.0
+INIT_STD_DEV = 1.0
+
 
 class ConceptInfoView(APIView):
     def get(self, request: Request, format=None) -> Response:
-        ks = InferredKnowledgeState.objects.get(
-            user=request.GET["user_id"], concept__cytoscape_id=request.GET["concept_id"]
+        user_id = request.GET["user_id"]
+        concept_id = request.GET["concept_id"]
+        ks = InferredKnowledgeState.objects.filter(
+            user=user_id, concept__cytoscape_id=concept_id
         ).select_related("concept__question_templates")
+        if not ks.exists():
+            ks = InferredKnowledgeState.objects.create(
+                user=user_id,
+                concept=concept_id,
+                mean=INIT_MEAN,
+                std_dev=INIT_STD_DEV,
+            )
         return Response(
             {
                 "level": ks.knowledge_level,
