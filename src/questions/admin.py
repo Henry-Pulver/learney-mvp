5670a201a59d171c3259e22f3207cf8ee1c087c0
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-30 11:55:11 +0000

    Add admin for other question-related models

--- src/questions/admin.py
+++ src/questions/admin.py
@@ -1,10 +1,63 @@
 from django.contrib import admin
 
-from questions.models import QuestionTemplate
+from questions.models import QuestionResponse, QuestionTemplate
+from questions.models.inferred_knowledge_state import InferredKnowledgeState
+from questions.models.question_set import QuestionSet
 
 
 @admin.register(QuestionTemplate)
-class QuestionTemplateModelAdmin(admin.ModelAdmin):
+class QuestionTemplateAdmin(admin.ModelAdmin):
     list_select_related = ("concept",)
     list_display = ("id", "concept", "question_type", "difficulty", "last_updated")
     search_fields = ["id", "question_type", "template_text", "difficulty", "concept"]
+
+
+@admin.register(QuestionSet)
+class QuestionSetAdmin(admin.ModelAdmin):
+    list_select_related = ("concept", "user")
+    list_display = (
+        "id",
+        "user",
+        "concept",
+        "time_started",
+        "time_taken_to_complete",
+        "level_at_start",
+        "levels_progressed",
+        "concept_completed",
+    )
+    search_fields = ["id", "user", "concept", "time_started", "time_taken_to_complete"]
+
+
+@admin.register(QuestionResponse)
+class QuestionResponseAdmin(admin.ModelAdmin):
+    list_select_related = ("user", "question_template", "question_set")
+    list_display = (
+        "id",
+        "user",
+        "question_template",
+        "question_params",
+        "question_set",
+        "correct",
+        "response",
+        "time_to_respond",
+        "time_asked",
+    )
+    search_fields = [
+        "id",
+        "user",
+        "question_template",
+        "question_params",
+        "question_set",
+        "response",
+        "concept",
+    ]
+
+
+@admin.register(InferredKnowledgeState)
+class InferredKnowledgeStateAdmin(admin.ModelAdmin):
+    list_select_related = (
+        "user",
+        "concept",
+    )
+    list_display = ("id", "user", "concept", "mean", "std_dev", "last_updated")
+    search_fields = ["id", "user", "concept"]
