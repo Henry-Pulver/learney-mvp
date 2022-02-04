8e8ab30230ee9b0e42f65b50c38e44fe68f7a15a
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-04 10:40:20 +0000

    Tweak message order and use template_id

--- src/questions/views/report_questions.py
+++ src/questions/views/report_questions.py
@@ -7,7 +7,6 @@ import boto3
 from botocore.exceptions import ClientError
 from learney_web.settings import AWS_CREDENTIALS, IS_PROD
 from questions.models import QuestionTemplate
-from questions.utils import uuid_and_params_from_frontend_id
 
 CHARSET = "UTF-8"
 
@@ -26,7 +25,8 @@ def get_admin_edit_link(question_template_id: str) -> str:
 
 class ReportBrokenQuestionView(APIView):
     def post(self, request: Request, format=None) -> Response:
-        template_id, params = uuid_and_params_from_frontend_id(request.data["question"]["id"])
+        template_id = request.data["question"]["template_id"]
+
         question_template = QuestionTemplate.objects.prefetch_related("concept").get(id=template_id)
         concept_name = question_template.concept.name
 
@@ -40,10 +40,11 @@ class ReportBrokenQuestionView(APIView):
         # The email body for recipients with non-HTML email clients.
         body_text = (
             f"Question on '{concept_name}' reported as broken!\r\n"
-            f"Params used: {params}\r\n"
             f"Issue type: {request.data['type']}\r\n"
-            f"Message: \n{request.data['message']}\r\n"
-            f"Link to edit: {admin_edit_link}"
+            f"User message: \n{request.data['message']}\r\n"
+            f"Params used: {request.data['question']['params']}\r\n"
+            f"Link to edit: {admin_edit_link}\r\n"
+            f"Question_text: \n\n{request.data['question']['question_text']}"
         )
 
         # The HTML body of the email.
@@ -52,13 +53,15 @@ class ReportBrokenQuestionView(APIView):
         <body>
           <h1>Question on '{concept_name}' reported as broken!</h1>
           <p>
-          <b>{request.data['type']}</b>
+          <b>Issue type: {request.data['type']}</b>
           <br/>
-          Params used: {params}\r\n
+          User message: \n{request.data['message']}
           <br/>
-          Message: \n{request.data['message']}
+          Params used: {request.data['question']['params']}\r\n
           <br/>
             <a href='{admin_edit_link}'>Here is the link to edit the question</a>.
+          <br/>
+          Question_text: \n\n{request.data['question']['question_text']}
           </p>
         </body>
         </html>
