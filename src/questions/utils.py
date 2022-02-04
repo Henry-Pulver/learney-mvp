ac89b64e74c0b1a1a4e48384336e75cca9bcd3b1
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-04 10:40:40 +0000

    Remove param + template_id functions
    No longer necessary!

--- src/questions/utils.py
+++ src/questions/utils.py
@@ -1,19 +1,8 @@
-import json
 from datetime import datetime
-from typing import Dict, Tuple
-from uuid import UUID
+from typing import Dict
 
 SampledParamsDict = Dict[str, str]
 
 
 def get_today() -> datetime:
     return datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-
-
-def get_frontend_id(template_id: UUID, params_dict: SampledParamsDict) -> str:
-    return f"{params_dict}|{template_id}"
-
-
-def uuid_and_params_from_frontend_id(frontend_id: str) -> Tuple[UUID, SampledParamsDict]:
-    params_string, template_id = frontend_id.split("|")
-    return UUID(template_id), json.loads(params_string.replace("'", '"')) if params_string else {}
