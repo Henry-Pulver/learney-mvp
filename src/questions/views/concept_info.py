3fda99450a9291177f59d0f41dabbefa1186045b
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-07 10:31:49 +0000

    Speed up getting user knowledge level with caching

--- src/questions/views/concept_info.py
+++ src/questions/views/concept_info.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,19 +17,23 @@ class ConceptInfoView(APIView):
     def get(self, request: Request, format=None) -> Response:
         user_id = request.GET["user_id"]
         concept_id = request.GET["concept_id"]
-        ks = InferredKnowledgeState.objects.filter(
-            user__id=user_id, concept__cytoscape_id=concept_id
-        ).prefetch_related("concept__question_templates")
-        if not ks.exists():
-            ks = InferredKnowledgeState.objects.create(
-                user=User.objects.get(id=user_id),
-                concept=Concept.objects.get(cytoscape_id=concept_id),
-                mean=INIT_MEAN,
-                std_dev=INIT_STD_DEV,
-                highest_level_achieved=GaussianParams(INIT_MEAN, INIT_STD_DEV).level,
-            )
-        else:
-            ks = ks[0]
+        ks = cache.get(f"InferredKnowledgeState:concept:{concept_id}user:{user_id}")
+
+        if ks is None:
+            ks = InferredKnowledgeState.objects.filter(
+                user__id=user_id, concept__cytoscape_id=concept_id
+            ).prefetch_related("concept__question_templates")
+            if not ks.exists():
+                ks = InferredKnowledgeState.objects.create(
+                    user=User.objects.get(id=user_id),
+                    concept=Concept.objects.get(cytoscape_id=concept_id),
+                    mean=INIT_MEAN,
+                    std_dev=INIT_STD_DEV,
+                    highest_level_achieved=GaussianParams(INIT_MEAN, INIT_STD_DEV).level,
+                )
+            else:
+                ks = ks[0]
+            cache.set(f"InferredKnowledgeState:concept:{concept_id}user:{user_id}", ks)
         return Response(
             {
                 "level": ks.knowledge_level,
