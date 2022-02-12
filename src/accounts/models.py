4fbd9fd60f01f8bd28a80b3ae9f58f0553727a70
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-12 17:19:29 +0000

    Set user fields to null to allow changing in admin panel

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -9,12 +9,12 @@ class User(models.Model):
     id = models.CharField(primary_key=True, max_length=255)
 
     name = models.CharField(max_length=255)
-    nickname = models.CharField(blank=True, max_length=255)
-    given_name = models.CharField(blank=True, max_length=255)
-    family_name = models.CharField(blank=True, max_length=255)
+    nickname = models.CharField(null=True, blank=True, max_length=255)
+    given_name = models.CharField(null=True, blank=True, max_length=255)
+    family_name = models.CharField(null=True, blank=True, max_length=255)
 
     picture = models.URLField(max_length=1000)
-    locale = models.CharField(blank=True, max_length=8)
+    locale = models.CharField(null=True, blank=True, max_length=8)
 
     checked_content_links = models.ManyToManyField(
         ContentLinkPreview,
