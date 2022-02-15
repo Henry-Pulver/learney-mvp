5542efed9159b528cb66103b40554e3d8dfe1e16
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-15 18:01:51 +0000

    Update streak & timezone on page_visit. Return streak

--- src/page_visits/views.py
+++ src/page_visits/views.py
@@ -1,6 +1,8 @@
 import datetime
+from typing import Any, Dict
 from uuid import uuid4
 
+from django.core.cache import cache
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -33,31 +35,55 @@ def update_session(request: Request) -> None:
     request.session.set_expiry(SESSION_EXPIRY_TIME)
 
 
-def get_or_generate_user_id(request: Request) -> str:
-    if request.data.get("user_id") is None:
-        return f"anonymous-user|{uuid4()}"
+def get_and_update_user(request: Request) -> Dict[str, Any]:
     user_id = request.data["user_id"]
-    # If user doesn't exist, add user!
-    if not User.objects.filter(id=user_id).exists():
-        serializer = UserSerializer(data=user_data_to_user_db_object(request.data["user_data"]))
-        if serializer.is_valid():
-            serializer.save()
-    return user_id
+    user = cache.get(f"user-{user_id}")
+    if user is None:
+        user_set = User.objects.filter(id=user_id)
+        # If user doesn't exist, add user!
+        if user_set.exists():
+            user = user_set.first()
+        else:
+            user_data = user_data_to_user_db_object(request.data["user_data"])
+            user = User.objects.create(**user_data)
+            cache.set(f"user-{user_id}", user, timeout=60 * 60 * 2)
+
+    if user.in_questions_trial:
+        # Update questions streak
+        today = user.question_batch_finished_today()
+        # If user did a batch yesterday, the streak hasn't been broken.
+        # If they didn't, it's a new streak or 0, depending on if they did it today.
+        streak = user.questions_streak if user.question_batch_finished_yesterday() else int(today)
+        # If statements speed up view by not saving in the DB unless necessary
+        if (
+            user.questions_streak != streak
+            or user.utc_tz_difference != request.data["user_data"]["utc_tz_difference"]
+        ):
+            user.questions_streak = streak
+            user.utc_tz_difference = request.data["user_data"]["utc_tz_difference"]
+            user.save()
+        return {"questions_streak": streak, "batch_completed_today": today}
+    return {}
 
 
 class PageVisitView(APIView):
     def post(self, request: Request, format=None):
         update_session(request)
+        user_id = request.data.get("user_id", f"anonymous-user|{uuid4()}")
+        user_data = get_and_update_user(request) if not user_id.startswith("anonymous-user") else {}
         serializer = PageVisitSerializer(
             data={
-                "user_id": get_or_generate_user_id(request),
+                "user_id": user_id,
                 "session_id": request.session.session_key,
                 "page_extension": request.data.get("page_extension"),
             }
         )
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            data_output = {**serializer.data}
+            data_output.update(user_data)
+            print(f"data_output: {data_output}")
+            return Response(data_output, status=status.HTTP_201_CREATED)
         else:
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
