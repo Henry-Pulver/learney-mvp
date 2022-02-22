ff570008d4728520a607688aaca0320dcadbb15d
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-22 14:22:18 +0000

    Refactor goals, learned and map views in line with .get() caching fn

--- src/goals/views.py
+++ src/goals/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
 from rest_framework.request import Request
@@ -6,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from goals.models import GoalModel
-from goals.serializers import GoalSerializer
 from knowledge_maps.models import KnowledgeMapModel
 from learney_web.settings import IS_PROD, mixpanel
 
@@ -14,33 +12,26 @@ from learney_web.settings import IS_PROD, mixpanel
 class GoalView(APIView):
     def get(self, request: Request, format=None):
         try:
-            entry = GoalModel.objects.filter(
-                user_id=request.GET["user_id"], map__unique_id=request.GET["map"]
-            ).latest("timestamp")
-            serializer = GoalSerializer(entry)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except ObjectDoesNotExist as error:
-            return Response(str(error), status=status.HTTP_204_NO_CONTENT)
+            user_id = request.GET["user_id"]
+            map_uuid = request.GET["map"]
+            goal = GoalModel.get(user_id, map_uuid)
+            if goal is None:
+                return Response(str("No goal found"), status=status.HTTP_204_NO_CONTENT)
+            return Response(goal.json(), status=status.HTTP_200_OK)
         except MultiValueDictKeyError as error:
             return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request: Request, format=None):
-        serializer = GoalSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        map_uuid = request.data.pop("map")
+        request.data["map"] = KnowledgeMapModel.objects.get(unique_id=map_uuid)
+        goal_model = GoalModel(**request.data)
+
         # Find the goal added or removed and whether it was added or removed
-        prev_entries = GoalModel.objects.filter(
-            user_id=request.data["user_id"], map=request.data["map"]
-        )
-        no_entries = prev_entries.count() == 0
-        if no_entries:
-            relevant_map = KnowledgeMapModel.objects.get(unique_id=request.data["map"])
-        else:
-            entry = prev_entries.latest("timestamp")
+        prev_entry = GoalModel.get(goal_model.user_id, map_uuid)
 
         # Work out the difference between the previously set goals and new goals
         new_goals_set = set(request.data["goal_concepts"])
-        prev_goals_set = set([]) if no_entries else set(entry.goal_concepts)
+        prev_goals_set = set(prev_entry.goal_concepts) if prev_entry is not None else set([])
 
         goal_added = new_goals_set - prev_goals_set
         goal_removed = prev_goals_set - new_goals_set
@@ -48,17 +39,15 @@ class GoalView(APIView):
         mixpanel_dict = {
             "Goal set": goal_added.pop() if goal_added else goal_removed.pop(),
             "Goal added or removed": "Added" if goal_added else "Removed",
-            "Map URL extension": relevant_map.url_extension
-            if no_entries
-            else entry.map.url_extension,
-            "Map Title": relevant_map.title if no_entries else entry.map.title,
-            "map_uuid": request.data["map"],
+            "Map URL extension": request.data["map"].url_extension,
+            "Map Title": request.data["map"].title,
+            "map_uuid": map_uuid,
             "session_id": request.data["session_id"],
         }
 
+        goal_model.save()
         # Track with mixpanel
         mixpanel.track(
             request.data["user_id"], "Set Goal" if IS_PROD else "Test Event", mixpanel_dict
         )
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(goal_model.json(), status=status.HTTP_201_CREATED)
--- src/learned/views.py
+++ src/learned/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
 from rest_framework.request import Request
@@ -7,56 +6,46 @@ from rest_framework.views import APIView
 
 from knowledge_maps.models import KnowledgeMapModel
 from learned.models import LearnedModel
-from learned.serializers import LearnedSerializer
 from learney_web.settings import IS_PROD, mixpanel
 
 
 class LearnedView(APIView):
     def get(self, request: Request, format=None):
         try:
-            entry = LearnedModel.objects.filter(
-                user_id=request.GET["user_id"], map__unique_id=request.GET["map"]
-            ).latest("timestamp")
-            serializer = LearnedSerializer(entry)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except ObjectDoesNotExist as error:
-            return Response(str(error), status=status.HTTP_204_NO_CONTENT)
+            user_id = request.query_params["user_id"]
+            map_uuid = request.query_params["map"]
+            learned = LearnedModel.get(user_id, map_uuid)
+            if learned is None:
+                return Response(str("No learned found"), status=status.HTTP_204_NO_CONTENT)
+            return Response(learned.json(), status=status.HTTP_200_OK)
         except MultiValueDictKeyError as error:
             return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request: Request, format=None):
-        serializer = LearnedSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(str(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+        map_uuid = request.data.pop("map")
+        request.data["map"] = KnowledgeMapModel.objects.get(unique_id=map_uuid)
+        learned_model = LearnedModel(**request.data)
 
         # Find the learned concepts added or removed and whether they were added or removed
-        prev_entries = LearnedModel.objects.filter(
-            user_id=request.data["user_id"], map__unique_id=request.data["map"]
-        )
-        no_entries = prev_entries.count() == 0
-        if no_entries:
-            relevant_map = KnowledgeMapModel.objects.get(unique_id=request.data["map"])
-        else:
-            entry = prev_entries.latest("timestamp")
+        prev_entry = LearnedModel.get(request.data["user_id"], map_uuid)
 
         new_learned_set = set(request.data["learned_concepts"])
-        prev_learned_set = set([]) if no_entries else set(entry.learned_concepts)
+        prev_learned_set = set(prev_entry.learned_concepts) if prev_entry is not None else set([])
         learned_added = list(new_learned_set - prev_learned_set)
         learned_removed = list(prev_learned_set - new_learned_set)
 
         mixpanel_dict = {
             "Learned set": learned_added or learned_removed,
             "Learned added or removed": "Added" if learned_added else "Removed",
-            "Map URL extension": relevant_map.url_extension
-            if no_entries
-            else entry.map.url_extension,
-            "Map Title": relevant_map.title if no_entries else entry.map.title,
-            "map_uuid": request.data["map"],
+            "Map URL extension": request.data["map"].url_extension,
+            "Map Title": request.data["map"].title,
+            "map_uuid": map_uuid,
             "session_id": request.data["session_id"],
         }
+        # Saving the model low down the function so we don't mess up the prev_entries query
+        learned_model.save()
         # Track with mixpanel
         mixpanel.track(
             request.data["user_id"], "Learned" if IS_PROD else "Test Event", mixpanel_dict
         )
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(learned_model.data, status=status.HTTP_201_CREATED)
