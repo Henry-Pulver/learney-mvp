1a8ce09b95105a11c74825a1935dfc218f51cbe1
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-22 14:15:36 +0000

    Add caching-based .get() method to Goals, Learned and Maps

--- src/learned/models.py
+++ src/learned/models.py
@@ -1,3 +1,7 @@
+from typing import Any, Dict, Optional, Union
+from uuid import UUID
+
+from django.core.cache import cache
 from django.db import models
 
 from knowledge_maps.models import KnowledgeMapModel
@@ -17,3 +21,30 @@ class LearnedModel(models.Model):
     )
     learned_concepts = models.JSONField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def cache_name(user_id: str, map_uuid: Union[str, UUID]) -> str:
+        return f"learned_{user_id}_{map_uuid}"
+
+    @staticmethod
+    def get(user_id: str, map_uuid: str) -> Optional["LearnedModel"]:
+        learned = cache.get(LearnedModel.cache_name(user_id, map_uuid))
+        if learned is None:
+            learned_queryset = LearnedModel.objects.filter(user_id=user_id, map=map_uuid)
+            learned = learned_queryset.latest("timestamp") if learned_queryset.exists() else None
+            cache.set(LearnedModel.cache_name(user_id, map_uuid), learned, 60 * 60 * 24)
+        return learned
+
+    def save(self, *args, **kwargs) -> None:
+        super(LearnedModel, self).save(*args, **kwargs)
+        cache.set(LearnedModel.cache_name(self.user_id, self.map.unique_id), self, 60 * 60 * 24)
+
+    def json(self) -> Dict[str, Any]:
+        return {
+            "pk": self.pk,
+            "map": self.map.unique_id,
+            "user_id": self.user_id,
+            "session_id": self.session_id,
+            "learned_concepts": self.learned_concepts,
+            "timestamp": self.timestamp,
+        }
