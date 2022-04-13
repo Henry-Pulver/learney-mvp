1cfd813a951a716ca1c31d1c50fcfe1b9bab27ab
Henry Pulver <henrypulver@hotmail.co.uk>
2022-04-13 09:33:33 +0100

    Add retry flag to author

--- src/learney_backend/models/content_author.py
+++ src/learney_backend/models/content_author.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
-from bs4.element import Tag
+from bs4 import BeautifulSoup
 from django.db import models
 
 from learney_backend.base_models import UUIDModel
-from learney_backend.utils import IMG_TAGS, meta_content_from_name_tags
+from learney_backend.utils import IMG_TAGS, get_base_url, meta_content_from_name_tags, url_regex
 from learney_backend.youtube_info_getter import get_youtube_channel_info
 
 
@@ -14,6 +14,7 @@ class ContentAuthor(UUIDModel):
     youtube_channel_id = models.CharField(
         max_length=100, blank=True, null=True, help_text="Youtube channel ID"
     )
+    retry_get_from_youtube = models.BooleanField(default=False)
 
     last_updated = models.DateTimeField(auto_now=True)
 
@@ -21,18 +22,27 @@ class ContentAuthor(UUIDModel):
         return {
             "id": self.id,
             "name": self.name,
-            "picture_url": self.image_url,
+            "image_url": self.image_url,
         }
 
     def populate_from_youtube(self) -> None:
         channel_info = get_youtube_channel_info(self.youtube_channel_id)
-        self.name = channel_info["snippet"]["title"]
-        self.image_url = channel_info["snippet"]["thumbnails"]["high"]["url"]
+        if channel_info is not None:
+            self.name = channel_info["snippet"]["title"]
+            self.image_url = channel_info["snippet"]["thumbnails"]["high"]["url"]
+        else:
+            self.retry_get_from_youtube = True
         self.save()
 
-    def populate_from_website(self, response_soup: Tag) -> None:
+    def populate_from_website(self, url: str, response_soup: BeautifulSoup) -> None:
         # Either get the image tag from the website or use the first <img> element on the page
-        self.image_url = (
-            meta_content_from_name_tags(response_soup, IMG_TAGS) or response_soup.find("img")["src"]
-        )
+        image_url_or_extension = meta_content_from_name_tags(
+            response_soup, IMG_TAGS
+        ) or response_soup.find("img").get("src", "")
+        self.image_url = ""
+        # Sometimes the img src is not absolute, so we need to prepend the url
+        if url_regex(image_url_or_extension) is None:  # Check if it's a relative url
+            self.image_url += get_base_url(url)
+        self.image_url += image_url_or_extension
+
         self.save()
